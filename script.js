195a6146bdeacaa304a2f78aa059f7b8f94cc0db
nthBit <207577365+nthBit@users.noreply.github.com>
2026-05-05 11:39:38 +0100

    Add dinosaur.png and some basic DOM changes

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-const container = document.querySelector("#container");
+// const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
-container.appendChild(content);
+// const content = document.createElement("div");
+// content.classList.add("content");
+// content.textContent = "This is the glorious text-content!";
+// container.appendChild(content);
 
-const paragraph = document.createElement("p");
-paragraph.textContent = "Hey I'm red!";
-paragraph.style.color = "red";
-container.appendChild(paragraph);
+// const paragraph = document.createElement("p");
+// paragraph.textContent = "Hey I'm red!";
+// paragraph.style.color = "red";
+// container.appendChild(paragraph);
 
-const subTitle = document.createElement("h3");
-subTitle.textContent = "Hey a blue h3!";
-subTitle.style.color = "blue";
-container.appendChild(subTitle);
+// const subTitle = document.createElement("h3");
+// subTitle.textContent = "Hey a blue h3!";
+// subTitle.style.color = "blue";
+// container.appendChild(subTitle);
 
-const specialContent = document.createElement("div");
-specialContent.style.backgroundColor = "pink";
-specialContent.style.border = "solid";
-container.appendChild(specialContent);
+// const specialContent = document.createElement("div");
+// specialContent.style.backgroundColor = "pink";
+// specialContent.style.border = "solid";
+// container.appendChild(specialContent);
 
-const mainTitle = document.createElement("h1");
-specialContent.appendChild(mainTitle);
-mainTitle.textContent = "I'm in a div";
+// const mainTitle = document.createElement("h1");
+// specialContent.appendChild(mainTitle);
+// mainTitle.textContent = "I'm in a div";
 
-const anotherParagraph = document.createElement("p");
-anotherParagraph.textContent = "ME TOO!";
-specialContent.appendChild(anotherParagraph);
+// const anotherParagraph = document.createElement("p");
+// anotherParagraph.textContent = "ME TOO!";
+// specialContent.appendChild(anotherParagraph);
+
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//     alert("Hello");
+//     alert("World");
+// });
+
+// btn.addEventListener("click", function (e) {
+//     console.log(e);
+// });
+
+// btn.addEventListener("click", function (e) {
+//   e.target.style.background = "blue";
+// });
+
+const link = document.querySelector("a");
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
